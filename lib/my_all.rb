362e3ebde337c406bb3df60983dbed9e69e71982
dwane richards <dwanerich@gmail.com>
2019-12-17 01:12:27 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,14 +1,6 @@
 require 'pry'
 
 def my_all?(collection)
-  counter = 0
-  new_array = []
-  while counter < collection.length do
-    new_array << yield(collection[counter])
-  end
-  if new_array.includes?(false)
-    return false
-  else
-    return true
-  end
+
+
 end
